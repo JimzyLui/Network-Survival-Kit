d7a175714374ff3d62ee04cf6bcbe63a2fc81d29
Jimzy Lui <jimzy.lui@gmail.com>
2019-08-06 03:33:50 -0400

    initial completion of clientBrowser.py

--- clientBrowser.py
+++ clientBrowser.py
@@ -9,22 +9,69 @@
 #       server info, and the html.
 
 import argparse
-import urllib
+import urllib.request
 
 
-# create parser
-parser = argparse.ArgumentParser(
-    prog="Network Survival Kit",
-    description="Command line networking tool kit"
-)
+def client_browser(url, verbose_tf):
+    url = check_url(url)
+    req = urllib.request.Request(url)
+    # print(req)
+    with urllib.request.urlopen(req) as obj_response:
+        # html = obj_response.read().decode("utf-8")
+        #print(html)
+        url_real = obj_response.geturl()
+        info = obj_response.info()
+        statuscode = obj_response.getcode()
+        header = obj_response.getheaders()
+        print('URL: {}'.format(url))
+        print(f'Real URL: {url_real}')
+        print('Request Status: {}'.format(statuscode))
+        # print_header(header)
+        # html = obj_response.read()
+        print(f'Request Header Info: {header}')
+        print(f'Server Info: {info}')
+        html = obj_response.read(9000).decode('utf-8')
+        print(f"HTML:\n{html}")
 
-parser.add_argument("url", nargs='?', default='', help="Get info from URL")
-# parser.add_argument("--protocol", choices=['tcp','icmp'], help="Network Protocol")
 
-args = parser.parse_args()
-print(args)
+def check_url(url):
+    if url[4] =='http':
+        return url
+    elif url[3]=='ftp':
+        return url
+    elif url[3]=='udp':
+        return url
+    else:
+        url = 'http://' + url
+        return url
 
-url = args.url or ''
 
-def client_browser(url):
-    pass
+def print_header(hdr):
+    # print(hdr)
+    print('Header: ')
+    for h in hdr:
+        print(" {h}".format(h))
+
+
+
+def print_rpt_line(port, status):
+    print("Port {}: {}".format(port, status))
+
+
+if __name__ == "__main__":
+    # create parser
+    parser = argparse.ArgumentParser(
+        prog="clientBrowser",
+        description="Pulls html from any domain",
+        add_help=True
+    )
+
+    parser.add_argument("url", help="Domain or IP address")
+    parser.add_argument("--verbose", action='store_true', help="Show processing details")
+    args = parser.parse_args()
+    verbose = args.verbose
+    if verbose:
+        print('url: {}'.format(args.url))
+                      
+    client_browser(args.url, verbose)
+
